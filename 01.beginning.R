5896352f6414d6f312a25ef3741f2a3301f9e330
SuadaPastacaldi <146854349+SuadaPastacaldi@users.noreply.github.com>
2023-10-03 17:57:02 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -6,3 +6,26 @@
 zina<-4+4
 duccio<-5+1
 final<- zina*duccio
+
+# Array
+sophie <- c(10,20,30,40) #number of units
+paula <- c(100,200,300,500) #number of people
+
+#Plot raphic representation of the link between the two
+plot(paula, sophie) graphic representation of the link between the two
+plot(paula, sophie, xlab="number of people", ylab="number of microplastics")
+#or
+paula<-people
+sophie<-microplastics
+plot(people, microplastic)
+# canging graphic layout
+plot(people, microplastic, pch=19)
+plot(people, microplastic, pch=19, cex=2)
+plot(people, microplastic, pch=19, cex=2, col="blue")
+
+# Instal packages
+instal.packages("sp")
+#to chack if it's actually there
+library(sp)
+
+# Functions got parenthesis and inside em there are #Arguments

84937f46975d9e1d3f58f8015660e957d79a7c31
SuadaPastacaldi <146854349+SuadaPastacaldi@users.noreply.github.com>
2023-10-05 17:29:50 +0200

    Create 02.populations.R

---
+++ 02.populations.R
@@ -0,0 +1,5 @@
+# code related to populations ecology
+# package needed for point pattern analysis is spatstat
+install.packages("spatstat") #quotes are needed every time you get out of R and back
+# to check if it's there
+library(spatstat) #no quotes needed cause inside R

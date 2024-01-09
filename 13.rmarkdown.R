4e1ce390a38c48acac5687153b0c38175f666916
SuadaPastacaldi <146854349+SuadaPastacaldi@users.noreply.github.com>
2024-01-09 17:40:49 +0100

    Create 13.rmarkdown.R

---
+++ 13.rmarkdown.R
@@ -0,0 +1,19 @@
+---
+title: "Markdown Report"
+output: html_document
+date: "2024-01-09"
+---
+
+# My first markdown document
+
+eval is "evaluation" if we want the text to be avaluated or not
+if it is-> code is in grey boxes and undernith we have the results in withe box
+
+```{r, eval=T}
+2+3
+```
+ 
+if not result 
+```{r, eval=F}
+2+3
+```

f578088bf53790477969fba39e12c919e471cf97
SuadaPastacaldi <146854349+SuadaPastacaldi@users.noreply.github.com>
2024-01-31 10:05:31 +0100

    Update 00.final.script.R

--- 00.final.script.R
+++ 00.final.script.R
@@ -697,7 +697,7 @@ im.plotRGB(stackg, r=1, g=2, b=4)
 
 #-------------------------------------------------------------------------
 
-#08 external data import
+#07 external data import
 
 
 ## EXTERNAL DATA IMPORT
@@ -803,4 +803,158 @@ plot(cut, col=c)
 
 #--------------------------------------------------------------------
 
-#09 classification
+#08 classification
+
+#Classify satellite images and estimate the amount of change
+
+#using frequencies of pixels, to see a change in time
+#vegetation --> no reflection of red it's absorbed, high reflectance in near infrared
+#water --> high reflection of red, near infrared absorbed
+
+#training sized
+#to explain the server what are the clusters (set of objects with the same characteristics)
+#ex. of clusters in our case ( lake, forest, city, agro field) more or less same kind of reflection in a particular area
+
+#minimum distance from class 
+#method to distinguish in which class a particular pixel is 
+#(for any kind of pixel in my image that is not a training size)
+
+####Classify
+library(terra)
+library(imageRy)
+library(ggplot2)
+
+install.packages("patchwork")
+library(patchwork)
+
+im.list()
+
+sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+#directly plotted by R 
+plotRGB(sun, 1, 2, 3)
+#3 levels of energy, black, yellow, white
+#created by the algorithm that selects pixels similar to each other
+#classify the image
+#im.classify(image, number of clusters)
+
+sun1<-im.classify(sun, num_clusters = 3)
+sun1
+
+
+##plot un 1
+plot(sun1)
+#gives tree images exactly the same replicated
+plot(sun1[[1]])
+
+#white has the highest energy down to yellow, green has the lowest
+
+
+#Mato grosso forest
+#apply it to the mato grosso forest to quantify the amount of change in the forest over time
+
+#import images
+im.list()
+#[21] "matogrosso_ast_2006209_lrg.jpg"                    
+#[22] "matogrosso_l5_1992219_lrg.jpg"   
+m1992<-im.import("matogrosso_l5_1992219_lrg.jpg")
+m2006<-im.import("matogrosso_ast_2006209_lrg.jpg" )
+
+#plotrgb
+plotRGB(m1992)
+#water in combination with the infrared shuld be black since it absorbs all infrared
+#in this image we can not distinguish 
+
+#create a classification with 2 clusters 1 for forest 1 for all others(water, urban, deforested)
+m1992c<-im.classify(m1992, 2)
+plot(m1992c[[1]])
+
+#CLASSES:
+#1=bare soil, water, 
+#2=forest
+
+#classify second image
+m2006c<-im.classify(m2006,2)
+plot(m2006c[[1]])
+#same classes
+
+par(mfrow=c(1,2))
+plot(m1992c[[1]])
+plot(m2006c[[1]])
+
+#quantify change
+#freq(image)
+#function calculating the frequency of a certain class
+
+f1992<-freq(m1992c)
+f2006<-freq(m2006c)
+f1992
+
+#compute percentage of pixel class
+#ncell() calculate n of pixels in an image
+ncell(m1992c)
+
+tot19992<-ncell(m1992c)
+
+#percentage
+#frequency/tot pixels * 100
+
+#1992
+p1992<-f1992/tot19992*100
+p1992
+#3 columns, percentages under "count"
+#       layer        value    count
+#1 5.555556e-05 5.555556e-05 16.91317
+#2 5.555556e-05 1.111111e-04 83.08683
+
+#forest=83%
+#humans=17%
+
+#2006
+tot2006<-ncell(m2006c)
+
+p2006<-f2006/tot2006*100
+p2006
+#forest=45%
+#humans=55%
+
+
+#graphical representation of data 
+#build a dataframe with all the data --> forest, human, percentages, 2006, 1992
+#function data.frame()
+
+#build columns of table
+class<- c("forest", "humans")
+y1992<-c(83,17)
+y2006<-c(45,55)
+
+tabella<-data.frame(class,y1992, y2006)
+tabella
+
+#to graph package ggplot2 needed
+library(ggplot2)
+
+# final plot
+#function ggplot(object, aes()+ geom_bar())
+#aes, aesthetics
+#fill, colour of filling
+
+
+# final plot
+p1 <- ggplot(tabella, aes(x=class, y=p1992, color=class)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(tabella, aes(x=class, y=p2006, color=class)) + geom_bar(stat="identity", fill="white")
+p1
+p2
+#[error] Don't know how to automatically pick scale for object of type <data.frame>. Defaulting
+#to continuous.
+#merge the 2 graphs
+p1+p2
+
+##different scales in the 2 graphs
+#rescale
+p1 <- ggplot(tabella, aes(x=class, y=y1992, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p2 <- ggplot(tabella, aes(x=class, y=y2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p1
+p2
+
+p1+p2
